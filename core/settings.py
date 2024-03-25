c5718b2308fd043eaf157b6bb676f9c39177d33e
Cooper <laobocooper@gmail.com>
2024-03-25 15:03:25 +0800

    update setting.py and dashboard.html
    setting.py  add STATICFILES_DIRS, 包含多个static文件夹

--- core/settings.py
+++ core/settings.py
@@ -164,6 +164,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 #path for user upload files
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
